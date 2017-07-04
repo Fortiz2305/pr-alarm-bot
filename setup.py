6850db284fb7cf4cad18c471e97326957b695b87
Francisco Ortiz <fortizabril@gmail.com>
2017-07-05 00:34:06 +0200

    Update version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
   name='pralarm',
   packages=['pralarm'],
-  version='0.1',
+  version='0.2',
   description='Slack bot which sends notifications when a GitHub organization\'s PR are older than a number of days',
   author='Francisco Ortiz',
   author_email='fortizabril@gmail.com',
   url='https://github.com/Fortiz2305/pr-alarm-bot',
-  download_url='https://github.com/Fortiz2305/pr-alarm-bot/archive/v0.1.tar.gz',
+  download_url='https://github.com/Fortiz2305/pr-alarm-bot/archive/v0.2.tar.gz',
   keywords=['slack', 'bot', 'github']
 )
